30c17bef75df90b8349b7161762bd0502fd2e222
Wisley César <wisleycesar32@gmail.com>
2024-04-04 17:55:07 -0300

    Projeto CAE

--- lib/Tela/home.dart
+++ lib/Tela/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:projetodepoo/Tela/components/atendimento.dart';
 
 main() => runApp(Myapp());
 
@@ -20,28 +20,150 @@ class Myhome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CAE'),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(17),
+            ),
+          ),
+          title: const Column(
+            children: [
+              Text(
+                'CAE',
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'IF-Goiano ',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: Color.fromARGB(219, 36, 123, 39)),
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+        ),
+        backgroundColor: Colors.amberAccent,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Card(
-            child: Text('Atendimento'),
-            elevation: 5,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Color.fromARGB(219, 36, 123, 39),
+        child: IconTheme(
+          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.settings),
+                ),
+                SizedBox(
+                  width: 45,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.home),
+                ),
+                SizedBox(
+                  width: 45,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.list),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+              ],
+            ),
           ),
-          Card(
-            child: Text('Nada Consta'),
-            elevation: 5,
-          ),
-          Card(
-            child: Text('Controle de Armario'),
-            elevation: 5,
-          ),
-          Card(
-            child: Text('Advertencia'),
-            elevation: 5,
-          ),
-        ],
+        ),
+      ),
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: ListView(children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amberAccent,
+                    elevation: 10,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AtendimentoPage()));
+                  },
+                  child: const Text(
+                    'Atendimento',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amberAccent,
+                    elevation: 10,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Nada Consta',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amberAccent, elevation: 10),
+                  onPressed: () {},
+                  child: const Text(
+                    'Controle Dos Armarios',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amberAccent),
+                  onPressed: () {},
+                  child: const Text(
+                    'Advertencia',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
