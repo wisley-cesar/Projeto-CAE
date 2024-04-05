5aaad57e8b86d06bc4bc12941b89dcc2da889b22
Wisley César <wisleycesar32@gmail.com>
2024-04-05 19:14:46 -0300

    Projeto CAE

--- lib/Tela/home.dart
+++ lib/Tela/home.dart
@@ -19,31 +19,7 @@ class Myhome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(17),
-            ),
-          ),
-          title: const Column(
-            children: [
-              Text(
-                'CAE',
-                style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'IF-Goiano ',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          backgroundColor: Color.fromARGB(219, 36, 123, 39)),
+      appBar: buildAppBar(),
       extendBody: true,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
@@ -53,118 +29,114 @@ class Myhome extends StatelessWidget {
         ),
         backgroundColor: Colors.amberAccent,
       ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        color: Color.fromARGB(219, 36, 123, 39),
-        child: IconTheme(
-          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
-                  width: 15,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.settings),
-                ),
-                SizedBox(
-                  width: 45,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.home),
-                ),
-                SizedBox(
-                  width: 45,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.list),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      body: Container(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: ListView(children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    elevation: 10,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const AtendimentoPage()));
-                  },
-                  child: const Text(
-                    'Atendimento',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    elevation: 10,
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Nada Consta',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.amberAccent, elevation: 10),
-                  onPressed: () {},
-                  child: const Text(
-                    'Controle Dos Armarios',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.amberAccent),
-                  onPressed: () {},
-                  child: const Text(
-                    'Advertencia',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ]),
-        ),
-      ),
+      bottomNavigationBar: buildBottomAppBar(context),
+      body: buildBody(context),
     );
   }
+}
+
+PreferredSize buildAppBar() {
+  return PreferredSize(
+    preferredSize: const Size.fromHeight(kToolbarHeight),
+    child: AppBar(
+      backgroundColor: Color(0xFF247B27),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(17),
+        ),
+      ),
+      title: const Column(
+        children: [
+          Text(
+            'CAE',
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'IF-Goiano',
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Widget buildBottomAppBar(BuildContext context) {
+  return BottomAppBar(
+    shape: const CircularNotchedRectangle(),
+    color: const Color(0xFF247B27),
+    child: IconTheme(
+      data: const IconThemeData(
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: 15),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings),
+            ),
+            const SizedBox(width: 45),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.home),
+            ),
+            const SizedBox(width: 45),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.list),
+            ),
+            const SizedBox(width: 15),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget buildBody(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(10),
+    child: ListView(
+      children: [
+        buildElvatedButton(context, 'Atendimento', const AtendimentoPage()),
+        buildElvatedButton(context, 'Nada Consta', null),
+        buildElvatedButton(context, 'Controle Dos Armarios', null),
+        buildElvatedButton(context, 'Advertencia', null),
+      ],
+    ),
+  );
+}
+
+Widget buildElvatedButton(BuildContext context, String texto, Widget? destino) {
+  return ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.amberAccent,
+    ),
+    onPressed: () {
+      if (destino != null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => destino),
+        );
+      }
+    },
+    child: Text(
+      texto,
+      style: const TextStyle(
+        fontSize: 23,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+    ),
+  );
 }
